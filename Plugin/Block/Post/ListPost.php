84d9e2d9f5ac5ba66851275d1a824f5f8bf78043
Dmitry Fedyuk <dfediuk@gmail.com>
2020-05-17 03:04:54 +0600

    https://github.com/tradefurniturecompany/blog/issues/1

---
+++ Plugin/Block/Post/ListPost.php
@@ -0,0 +1,18 @@
+<?php
+namespace TFC\Blog\Plugin\Block\Post;
+use FishPig\WordPress\Block\Post\ListPost as Sb;
+// 2020-05-17
+// "Implement a custom design for the `post/list.phtml` template":
+// https://github.com/tradefurniturecompany/blog/issues/1
+final class ListPost {
+	/**
+	 * 2020-05-17
+	 * @see \FishPig\WordPress\Block\Post\ListPost::getCustomBlogThemeVendor():
+	 *		function getCustomBlogThemeVendor() {return false;}
+	 * https://github.com/bentideswell/magento2-wordpress-integration/blob/2.0.11.19/Block/Post/ListPost.php#L120-L123
+	 * @param Sb $sb
+	 * @param string $r
+	 * @return string
+	 */
+	function afterGetCustomBlogThemeVendor(Sb $sb, $r) {return df_module_name($this);}
+}
